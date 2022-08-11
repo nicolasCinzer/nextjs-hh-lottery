9a525c614f5bce19983e5f9bebc2ef8fd6d915e7
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-10 23:56:12 -0300

    Added LoterryEntrance component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
+import LotteryEntrance from '../components/LotteryEntrance'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Decentralized Raffle</title>
-        <meta name="description" content="Smart contract raffle" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Smart contract raffle' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
-      Hi
+      <LotteryEntrance />
     </div>
   )
 }
